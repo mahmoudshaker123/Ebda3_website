30c285e23c3ed4db3440cb57bc3fbaeb9ee81890
mahmoudshaker123 <mahmoud.shaker123123@gmail.com>
2025-02-07 17:44:18 +0200

    before jazzmin admin

--- core/ebda3/urls.py
+++ core/ebda3/urls.py
@@ -4,3 +4,14 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
 ]
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('home/', views.index, name='home'),
+    path('gallery/', views.index, name='gallery'),
+    path('offers/', views.index, name='offers'),
+    path('about-us/', views.index, name='about-us'),
+    path('contact-us/', views.index, name='contact-us'),
+]
--- core/ebda3/views.py
+++ core/ebda3/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from .models import *
 
+
+
 def index(request):
+    section = request.path.strip('/')  # تحديد القسم بناءً على URL
     home = Home.objects.last()
     offers = Offer.objects.all()
     about = About.objects.last()
     contact_us = ContactUs.objects.last()
-    gallery = BusinessGallery.objects.all().order_by('-created_at')[:50]  # ترتيب الصور حسب تاريخ الإنشاء
-    return render(request, 'index.html', {'home': home, 'offers': offers, 'about': about , 'gallery': gallery , 'contact_us': contact_us})
+    gallery = BusinessGallery.objects.all().order_by('-created_at')[:50]
+
+    context = {
+        'home': home,
+        'offers': offers,
+        'about': about,
+        'gallery': gallery,
+        'contact_us': contact_us,
+        'section': section,  # لإرسال القسم المطلوب للقالب
+    }
+    return render(request, 'index.html', context)
